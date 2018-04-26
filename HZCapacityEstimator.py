c1082c70a0c0fd86ef5f23e7eadab618e6245677
Jeremy Lane <lane203j@gmail.com>
2018-04-26 17:50:48 +0200

    Created HZCapacityEstimator.py

---
+++ HZCapacityEstimator.py
@@ -0,0 +1,176 @@
+# HZCapacityEstimator class
+
+import numpy as np
+from math import sqrt, fabs
+
+# currently, hardcoded to estimate the Hofer-Zehnder capacity of 
+# the unit ball, which is pi.
+#
+# Goal: adapt to estimate the capacity of any convex (rational) polytope
+
+class HZCapacityEstimator:
+    
+    def __init__(self, n, m):
+        if n >=1:
+            self.n = n
+        else: n=1
+        if m >=3:
+            self.m = m
+        else: m=3
+        
+        Z = np.zeros((n,n))
+        I = np.identity(n)
+        self.J = np.block([[Z,I],[-I,Z]])
+
+    def H(self,x):  
+        # Assume x is a vector in R^2n
+        return np.sum(x*x)
+
+    def dH(self,x):
+        # Assume x is a vector in R^2n
+        return 2*x
+
+    def dG(self,y):
+        # Assume x is a vector in R^2n
+        # Computes dH inverse = dG
+        return y/2
+
+    def G(self,y):
+        # Assume y is a vector in R^2n
+        x = self.dG(y)   
+        return np.sum(y*x) - self.H(x)
+
+    def proj(self,x):
+        #   Orthogonal projection to the subspace H
+        #   described in section 2.3 of the paper, equation (2.17)
+        #   Assume x is a 2n x m matrix
+
+        s = np.sum(x,axis = 1,keepdims=True)
+
+        return x - s/self.m
+
+    def F(self,x):
+        #   Assume x is a 2n x m matrix
+
+        A = -np.dot(self.J,x)
+        S = 0.
+        for i in range(0,self.m):
+            S += self.G(A[:,i])
+
+        return S/self.m
+
+    def dF(self,x):
+        #   Assume x is a 2n x m matrix
+
+        A = -np.dot(self.J,x)
+
+        for i in range(0,self.m):
+            A[:,i] = np.dot(self.J, self.dG(A[:,i])) 
+
+        return A/self.m 
+
+    def A2n(self,x):
+        # Compute the product A2n x 
+        # (see paper sec. 2.1 equation (2.5))
+
+        A = -np.dot(self.J,x)
+        b = np.zeros(x.shape)
+
+        for k in range (0,self.m):
+            for j in range (k+1,self.m):
+                b[:,k] += A[:,j]
+
+        return b
+    
+    def f(self,x):
+        # Compute f(x) 
+        # (see paper sec. 2.1 equation (2.5))
+
+        return np.sum(x*self.A2n(x))/(self.m**2) - 1
+
+    def df(self,x):
+        # Compute the product (A2n + A2n^T)x /m^2
+        # (see G-J sec. 2.1 equation (2.11))
+
+        return (self.A2n(x) + np.flip(self.A2n(np.flip(x,axis=1)),axis=1))/(self.m**2)
+    
+    def randomStartingPoint(self,epsilon):
+        c = 0.
+        while c < epsilon:
+            x = 4*(0.5-np.random.rand(2*self.n,self.m))
+            x = self.proj(x)
+            c = self.f(x)+1
+        
+        if c > 0:
+            return x/sqrt(c)
+        else:
+            return -x/sqrt(-c)
+
+    def estimate(self, iterations = 10, epsilon = 10.**(-5)):
+        # Estimates the Hofer-Zehnder capacity of the convex body using 
+        # projected gradients to solve the convex constrained optimization 
+        # problem (see the thesis of GJ for details).
+       
+        k = 0
+        
+        # create random x_0 in the space M_m
+        x = self.randomStartingPoint(epsilon)
+        
+        while k < iterations:
+
+            k = k + 1
+            print("At k =", k, ", F(x) =", self.F(x))
+            if k % 10 == 0: print(k)
+
+            # Part 1
+
+            a     = self.df(x)                                        # calculate a_k
+            y     = -self.dF(x)                                       # calculate y_k
+            a_H   = self.proj(a)                                      # calculate a_kH
+            y_hat = self.proj(y) - a_H*np.sum(a_H*y)/np.sum(a_H*a_H)  # calculate y_k hat
+
+            # Part 2
+
+            if np.sum(y_hat*y_hat) < epsilon**2:
+                print("Early stop at iteration ", k)
+                print("The estimated Hofer-Zhender capacity of the unit ball is ", self.F(x))
+                return 
+
+            # Part 3
+
+            else:
+                L_max   = self.m * sqrt( 3/fabs( np.sum( y_hat* self.A2n(y_hat))))/2
+                h_kLmax = np.sum(-y_hat * self.dF(x+L_max*y_hat))
+                h_k0    = np.sum(y_hat*y_hat)
+
+                if h_kLmax >= 0:
+                    L_0  = L_max
+                else:
+                    L_0  = L_max * h_k0/(h_k0-h_kLmax)
+
+                carryOn = True
+                
+                while carryOn:
+
+                    c_L0  = (L_0**2)/(self.m**2)*np.sum(y_hat * self.A2n(y_hat)) + 1
+                    x_L0  = x + (L_0 * y_hat)
+                    x_ML0 = x_L0/sqrt(c_L0)
+                    del_1 = self.F(x) - self.F(x_L0)
+                    del_2 = self.F(x) - self.F(x_ML0)
+
+                    if 4*del_2 <= del_1:
+                        
+                        L_0 = L_0/2
+                    else:
+                        carryOn2 = False
+                        x        = x_ML0
+
+        print("The estimated Hofer-Zhender capacity of the unit ball is ", self.F(x))
+        # self.plot(x)
+        return 
+
+    def plot(self,x):
+    
+        return
+
+
